c923ae57f6dc2df93a5ca0fdd2dbec478a3d3fa7
jamesshieh <randomletters13@gmail.com>
2012-11-02 11:01:20 -0400

    completed kruskal in ruby

--- kruskal.rb
+++ kruskal.rb
@@ -1,4 +1,7 @@
+require 'debugger'
+
 class Kruskal
+  attr_accessor :mst
 
   Edge = Struct.new(:vertices, :cost)
 
@@ -6,27 +9,40 @@ class Kruskal
     @mst = []
     @edges = []
     @unmapped_nodes = []
+    @costs = {}
+    @mstsets = []
     graph.each do |v1, v2, c|
       @edges << Edge.new([v1, v2],c)
-      @unmapped_nodes << v1 unless @unmapped_nodes.include?(v1)
-      @unmapped_nodes << v2 unless @unmapped_nodes.include?(v2)
-    end
-    @costs = {}
-    @edges.each do |e|
-      @costs[e.cost] = [] if !@costs.has_key?[e.cost]
-      @costs[e.cost] << e.vertices
+      @unmapped_nodes << v1 if !@unmapped_nodes.include?(v1)
+      @unmapped_nodes << v2 if !@unmapped_nodes.include?(v2)
+      @costs[c] = [] if !@costs.has_key?(c)
+      @costs[c] << [v1,v2]
     end
   end
 
+  def find(x)
+    @mstsets.each do |set|
+      return set if set.include?(x)
+    end
+    return [x]
+  end
+
+  def new_edge?(edge)
+    @mstsets.each do |set|
+      return !(set.include?(edge[0]) && set.include?(edge[1]))
+    end
+  end
 
   def generate_mst
-    generator = @costs.sort.each
-    mstnodes = []
-    generator.each do |cost, edges|
-      for edges.each do |edge|
-        if mstnodes.include?(edge[0]) && mstnodes.include?(edge[1])
-          @mst << Edge.new(edges,cost)
-          mstnodes << edge[0], edge[1]
+    @costs.sort.each do |cost, edges|
+      edges.each do |edge|
+        if self.new_edge?(edge)
+          set1 = find(edge[0])
+          set2 = find(edge[1])
+          @mstsets.delete(set1)
+          @mstsets.delete(set2)
+          @mstsets << (set1 | set2)
+          @mst << Edge.new(edge,cost)
         end
       end
     end
--- kruskal_test.rb
+++ kruskal_test.rb
@@ -1,9 +1,15 @@
-require 'kruskal'
+require './kruskal'
+require 'tracer'
 
-graph = (
-
-
-
+graph = [
+  ['a','b',1],
+  ['b','c',1],
+  ['a','c',2],
+  ['a','d',3],
+  ['d','e',2]
+]
+Tracer.on
 kruskal = Kruskal.new(graph)
 kruskal.generate_mst
 puts kruskal.mst
+Tracer.off
